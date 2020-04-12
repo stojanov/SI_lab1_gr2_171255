c2cd13c2671f198a9f5e8850424248fb45b485d4
Angel Stojanov <stojanovangel@outlook.com>
2020-04-12 18:20:42 +0200

    Added point features

--- Point.java
+++ Point.java
@@ -3,18 +3,55 @@ class Point {
 	String color;
 	double x,y;
 
-	//TODO add new variable
+	Point(String _id, String _clr, double _x, double _y) {
+		id = _id;
+		color = _clr;
+		x = _x;
+		y = _y;
+	}
 
-	//TODO constructor
+	void setId(String _id)
+	{
+		id = _id;
+	}
 
-	//TODO setters and getters
+	void setColor(String _clr)
+	{
+		color = _clr;
+	}
+
+	void setX(double _x)
+	{
+		x = _x;
+	}
+
+	void setY(double _y)
+	{
+		y = _y;
+	}
+
+	void getId() { return id; }
+	void getColor() { return color; }
+	void getX() { return x; }
+	void getY() { return y; }
 
 	public void move (char xDirection, char yDirection) {
-		//TODO
+		switch (xDirection)
+		{
+			case 'L': x--; break;
+			case 'R': x++; break;
+			default: break;
+		}
+		switch (yDirection)
+		{
+			case 'U': y++; break;
+			case 'D': y--; break;
+			default: break;
+		}
 	}
 
 	public void draw () {
-		//TODO
+		System.out.println("(x, y): " + x ", " + y);
 	}
 
 
